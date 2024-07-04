5a63bd1904d6206ba95789583a04195a2308d99e
Clement Hugo <118815063+hugoclmt@users.noreply.github.com>
2024-07-05 01:35:41 +0200

    First commit

---
+++ pokerJava/src/main/java/org/example/Card.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Card {
+
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank) {
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+
+}
---
+++ pokerJava/src/main/java/org/example/Rank.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Rank {
+    AS, Deux, Trois, Quatre, Cinq, Six, Sept, Huit, Neuf, Dix, Valet, Dame, Rois
+}
